2c2d5f81eb5c5f94a88220848f239dbdca0d1305
Jaechan <dlwocks31@gmail.com>
2023-10-13 16:40:30 +0900

    adsf

--- content/graph/MinCostMaxFlow.h
+++ content/graph/MinCostMaxFlow.h
@@ -5,73 +5,72 @@
  * Description: Min cost max flow
  * Time: Fast
  */
-const int MAXN = 1010;
-struct MCMF
-{
-	struct edg { int pos, cap, rev, cost; };
-	vector<edg> gph[MAXN];
-	void clear(){for(int i=0; i<MAXN; i++) gph[i].clear();}
-
-	void add_edge(int s, int e, int cap, int cst)
-	{
-		gph[s].push_back({e, cap, (int)gph[e].size(), cst});
-		gph[e].push_back({s, 0, (int)gph[s].size()-1, -cst});
-	}
-
-	int dist[MAXN], pa[MAXN], pe[MAXN];
-	bool inque[MAXN];
-	bool spfa(int src, int sink)
-	{
-		memset(dist, 0x3f, sizeof(dist));
-		memset(inque, 0, sizeof(inque));
-		queue<int> que;
-		dist[src] = 0;
-		inque[src] = 1;
-		que.push(src);
-		bool ok = 0;
-		while(!que.empty())
-		{
-			int x = que.front();
-			que.pop();
-			if(x == sink) ok = 1;
-			inque[x] = 0;
-			for(int i = 0; i<gph[x].size(); i++)
-			{
-				edg e = gph[x][i];
-				if(e.cap > 0 && dist[e.pos] > dist[x] + e.cost)
-				{
-					dist[e.pos] = dist[x] + e.cost;
-					pa[e.pos] = x;
-					pe[e.pos] = i;
-					if(!inque[e.pos])
-					{
-						inque[e.pos] = 1;
-						que.push(e.pos);
-					}
-				}
-			}
-		}
-		return ok;
-	}
-	pii solveMCMF(int src, int sink)
-	{
-		int MCMF_COST = 0;
-		int MCMF_FLOW = 0;
-		while(spfa(src, sink))
-		{
-			int cap = 1e9;
-			for(int pos = sink; pos != src; pos = pa[pos])
-				cap = min(cap, gph[pa[pos]][pe[pos]].cap);
-
-			MCMF_COST += dist[sink] * cap;
-			for(int pos = sink; pos != src; pos = pa[pos])
-			{
-				int rev = gph[pa[pos]][pe[pos]].rev;
-				gph[pa[pos]][pe[pos]].cap -= cap;
-				gph[pos][rev].cap += cap;
-			}
-			MCMF_FLOW += cap;
-		}
-		return {MCMF_FLOW, MCMF_COST};
-	}
+struct Edge{
+    int u, v, w, cap, flow;
+};
+const int INF = 0x3f3f3f3f;
+struct MCMF {
+    int n;
+    vector<Edge> edges;
+    vector<vector<int>> adj;
+    vector<int> par, dist, inque;
+    MCMF(int _n) {
+        n = _n;
+        adj.resize(n+1);
+        par.resize(n+1);
+        dist.resize(n+1);
+        inque.resize(n+1);
+    }
+    void addedge(int u, int v, int cap, int w) {
+        adj[u].push_back(edges.size());
+        edges.push_back({u, v, w, cap, 0});
+        adj[v].push_back(edges.size());
+        edges.push_back({v, u, -w, cap, cap});
+    }
+    void spfa(int src) {
+        fill(par.begin(), par.end(), -1);
+        fill(dist.begin(), dist.end(), INF);
+        fill(inque.begin(), inque.end(), 0);
+        dist[src] = 0;
+        queue<int> que({src});
+        while(que.size()) {
+            int q = que.front();
+            que.pop();
+            inque[q] = 0;
+            for(int i: adj[q]) {
+                Edge& e = edges[i];
+                if(e.flow < e.cap && dist[e.v] > dist[e.u]+e.w) {
+                    dist[e.v] = dist[e.u]+e.w;
+                    par[e.v] = i;
+                    if(!inque[e.v]) {
+                        inque[e.v] = 1;
+                        que.push(e.v);
+                    }
+                }
+            }
+        }
+    }
+    pair<int, int> solve(int src, int sink) {
+        int mc = 0, mf = 0;
+        while(1) {
+            spfa(src);
+            if(par[sink] == -1)
+                return {mc, mf};
+            int flow = INF, c = sink;
+            while(c != src) {
+                Edge &e = edges[par[c]];
+                flow = min(flow, e.cap-e.flow);
+                c = e.u;
+            }
+            c = sink;
+            while(c != src) {
+                Edge &e = edges[par[c]], &ie = edges[par[c]^1];
+                e.flow += flow;
+                ie.flow -= flow;
+                c = e.u;
+            }
+            mf += flow;
+            mc += dist[sink]*flow;
+        }
+    }
 };
