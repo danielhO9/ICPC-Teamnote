5c2cf6d42870fb4280e30374c8d9427ba12f5b52
Jaechan <dlwocks31@gmail.com>
2024-10-01 13:55:26 +0900

    change 2sat

--- content/graph/2sat.h
+++ content/graph/2sat.h
@@ -1,75 +1,82 @@
 /**
- * Author: Emil Lenngren, Simon Lindholm
- * Date: 2011-11-29
- * License: CC0
- * Source: folklore
- * Description: Calculates a valid assignment to boolean variables a, b, c,... to a 2-SAT problem, so that an expression of the type $(a\|\|b)\&\&(!a\|\|c)\&\&(d\|\|!b)\&\&...$ becomes true, or reports that it is unsatisfiable.
- * Negated variables are represented by bit-inversions (\texttt{\tilde{}x}).
- * Usage:
- *  TwoSat ts(number of boolean variables);
- *  ts.either(0, \tilde3); // Var 0 is true or var 3 is false
- *  ts.setValue(2); // Var 2 is true
- *  ts.atMostOne({0,\tilde1,2}); // <= 1 of vars 0, \tilde1 and 2 are true
- *  ts.solve(); // Returns true iff it is solvable
- *  ts.values[0..N-1] holds the assigned values to the vars
- * Time: O(N+E), where N is the number of boolean variables, and E is the number of clauses.
- * Status: stress-tested
+ * Author: dlwocks31
  */
 #pragma once
 
-struct TwoSat {
-	int N;
-	vector<vi> gr;
-	vi values; // 0 = false, 1 = true
-
-	TwoSat(int n = 0) : N(n), gr(2*n) {}
-
-	int addVar() { // (optional)
-		gr.emplace_back();
-		gr.emplace_back();
-		return N++;
-	}
-
-	void either(int f, int j) {
-		f = max(2*f, -1-2*f);
-		j = max(2*j, -1-2*j);
-		gr[f].push_back(j^1);
-		gr[j].push_back(f^1);
-	}
-	void setValue(int x) { either(x, x); }
-
-	void atMostOne(const vi& li) { // (optional)
-		if (sz(li) <= 1) return;
-		int cur = ~li[0];
-		rep(i,2,sz(li)) {
-			int next = addVar();
-			either(cur, ~li[i]);
-			either(cur, next);
-			either(~li[i], next);
-			cur = ~next;
-		}
-		either(cur, ~li[1]);
-	}
-
-	vi val, comp, z; int time = 0;
-	int dfs(int i) {
-		int low = val[i] = ++time, x; z.push_back(i);
-		for(int e : gr[i]) if (!comp[e])
-			low = min(low, val[e] ?: dfs(e));
-		if (low == val[i]) do {
-			x = z.back(); z.pop_back();
-			comp[x] = low;
-			if (values[x>>1] == -1)
-				values[x>>1] = x&1;
-		} while (x != i);
-		return val[i] = low;
-	}
-
-	bool solve() {
-		values.assign(N, -1);
-		val.assign(2*N, 0); comp = val;
-		rep(i,0,2*N) if (!comp[i]) dfs(i);
-		rep(i,0,N) if (comp[2*i] == comp[2*i+1]) return 0;
-		return 1;
-	}
+struct SAT {
+    vector<vector<int>> adj;
+    vector<int> val, num, low, stk, scc, visiting;
+    int nxt, n, cnt;
+    int f(int i) {
+        if(i < 0) i = -i+n;
+        return i;
+    }
+    int neg(int i) {
+        if(i >= n)
+            return i-n;
+        return i + n;
+    }
+    SAT(int _n, vector<pair<int, int>>& edges) {
+        n = _n+1;
+        adj.resize(2*n+1);
+        val.resize(2*n+1);
+        num.resize(2*n+1);
+        low.resize(2*n+1);
+        scc.resize(2*n+1);
+        visiting.resize(2*n+1);
+        nxt = 1;
+        for(auto [a, b]: edges) {
+            a = f(a), b = f(b);
+            adj[neg(a)].push_back(b);
+            adj[neg(b)].push_back(a);
+        }
+        compute();
+    }
+    void compute() {
+        for(int i=1; i<=n; i++) {
+            if(!num[i])
+                dfs(i);
+        }
+        for(int i=n+1; i<=n+n; i++) {
+            if(!num[i])
+                dfs(i);
+        }
+    }
+    void dfs(int i) {
+        num[i] = low[i] = nxt++;
+        visiting[i] = 1;
+        stk.push_back(i);
+        for(int a: adj[i]) {
+            if(!num[a]) {
+                dfs(a);
+            }
+            if(visiting[a]) {
+                low[i] = min(low[i], low[a]);
+            }
+        }
+        if(num[i] == low[i]) {
+            int c = cnt++;
+            while(1) {
+                int t = stk.back();
+                stk.pop_back();
+                scc[t] = c;
+                visiting[t] = 0;
+                if(t == i) break;
+            }
+        }
+    }
+    bool ok() {
+        for(int i=1; i<=n; i++) {
+            if(scc[i] == scc[neg(i)])
+                return 0;
+        }
+        return 1;
+    }
+    vector<int> result() {
+        vector<int> ans(n+1);
+        for(int i=1; i<=n; i++) {
+            ans[i] = (scc[i] < scc[neg(i)]);
+        }
+        return ans;
+    }
 };
