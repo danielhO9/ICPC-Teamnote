ee83026e3589ea1b50ec0f87358f3f55ff4ddd61
daniel-604 <daniel_604@naver.com>
2025-07-24 22:20:08 +0900

    modify PST

--- content/data-structures/PersistentSegmentTree.h
+++ content/data-structures/PersistentSegmentTree.h
@@ -4,33 +4,25 @@
  * Description: Interval incremental modification, interval sum query on $[l, r]$.
  * Time: O(\log N)
  */
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-
-struct Node {
-	int l, r;
-	ll val;
-};
+struct Node { int l, r; ll val; };
 
 struct PST {
-	vector<Node> tree = {{0, 0, 0}, {0, 0, 0}};
-	vector<int> root;
+	vector<Node> tree;
+	vector<int> root; // 1-index (0: dummy)
 	int sz;
-	int ridx; // current root = root[ridx]
 
-	void init(int sz_) {
-		sz = sz_;
-		ridx = 1;
-		root.resize(sz);
-		root[0] = 1;
+	PST() = default;
+	PST(int sz): sz(sz) {
+        tree.resize(1);
+        root.resize(1);
 	}
-	void changeRoot(int idx) { ridx = root[idx]; }
-	
-	void newRoot(int idx) {
-        root[idx] = tree.size();
-		tree.push_back({tree[ridx].l, tree[ridx].r, tree[ridx].val});
-		ridx = root[idx];
+    // p에서 c로 분기
+	void newRoot(int c, int p) {
+        assert(p < root.size());
+        while (root.size() <= c) root.push_back(0);
+        root[c] = tree.size();
+		tree.push_back(Node{tree[root[p]]});
+        return;
 	}
 	void update(int node, int start, int end, int index, ll val) {
 		if (index < start || index > end) return;
@@ -40,32 +32,41 @@ struct PST {
 		if (index <= mid) {
 			int lidx = tree[node].l;
 			tree[node].l = tree.size();
-			tree.push_back({tree[lidx].l, tree[lidx].r, tree[lidx].val});
+			tree.push_back(Node{tree[lidx]});
 			update(tree[node].l, start, mid, index, val);
 		}
 		if (index > mid) {
 			int ridx = tree[node].r;
 			tree[node].r = tree.size();
-			tree.push_back({tree[ridx].l, tree[ridx].r, tree[ridx].val});
+			tree.push_back(Node{tree[ridx]});
 			update(tree[node].r, mid + 1, end, index, val);
 		}
-	}
-	void update(int rt, int index, ll val) {
-		update(root[rt], 0, sz - 1, index, val);
 	}
 	ll query(int node, int start, int end, int left, int right) {
 		if (left > end || right < start) return 0;
 		if (left <= start && end <= right) return tree[node].val;
-		ll lsum = query(tree[node].l, start, (start + end) / 2, left, right);
-		ll rsum = query(tree[node].r, (start + end) / 2 + 1, end, left, right);
+        int mid = (start + end) / 2;
+		ll lsum = query(tree[node].l, start, mid, left, right);
+		ll rsum = query(tree[node].r, mid + 1, end, left, right);
 		return lsum + rsum;
 	}
+    int kth(int l, int r, int start, int end, ll k) {
+        if (start == end) return start;
+        ll tmp = tree[tree[r].l].val - tree[tree[l].l].val;
+        int mid = (start + end) / 2;
+        if (tmp >= k) return kth(tree[l].l, tree[r].l, start, mid, k);
+        else return kth(tree[l].r, tree[r].r, mid + 1, end, k - tmp);
+    }
+    void update(int rt, int index, ll val) {
+        assert(rt < root.size());
+        update(root[rt], 0, sz - 1, index, val);
+    }
 	ll query(int rt, int left, int right) {
-		return query(root[rt], 0, sz - 1, left, right);
-	}
-} seg;
-
-void solve() {
-	// root(vector) size
-	seg.init(1e5 + 1);
-}
+        assert(rt < root.size());
+        return query(root[rt], 0, sz - 1, left, right);
+    }
+    int kth(int l, int r, int k) {
+        assert(l - 1 < root.size() && r < root.size());
+        return kth(root[l - 1], root[r], 0, sz - 1, k);
+    }
+};
