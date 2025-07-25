2f560e413144617f9610eb0701330f0b9cb0c7b5
daniel-604 <daniel_604@naver.com>
2025-07-25 12:36:51 +0900

    modify BCC

--- content/graph/BiconnectedComponents.h
+++ content/graph/BiconnectedComponents.h
@@ -1,54 +1,37 @@
 /**
- * Author: Simon Lindholm
- * Date: 2017-04-17
- * License: CC0
- * Source: folklore
- * Description: Finds all biconnected components in an undirected graph, and
- *  runs a callback for the edges in each. In a biconnected component there
- *  are at least two distinct paths between any two nodes. Note that a node can
- *  be in several components. An edge which is not in a component is a bridge,
- *  i.e., not part of any cycle.
+ * Author: koosaga
+ * Source: https://gist.github.com/koosaga/6f6fd50dd7067901f1b1
  * Time: O(E + V)
- * Status: tested during MIPT ICPC Workshop 2017
- * Usage:
- *  int eid = 0; ed.resize(N);
- *  for each edge (a,b) {
- *    ed[a].emplace_back(b, eid);
- *    ed[b].emplace_back(a, eid++); }
- *  bicomps([\&](const vi\& edgelist) {...});
  */
-#pragma once
 
-vi num, st;
-vector<vector<pii>> ed;
-int Time;
-template<class F>
-int dfs(int at, int par, F& f) {
-	int me = num[at] = ++Time, e, y, top = me;
-	for (auto pa : ed[at]) if (pa.second != par) {
-		tie(y, e) = pa;
-		if (num[y]) {
-			top = min(top, num[y]);
-			if (num[y] < me)
-				st.push_back(e);
-		} else {
-			int si = sz(st);
-			int up = dfs(y, e, f);
-			top = min(top, up);
-			if (up == me) {
-				st.push_back(e);
-				f(vi(st.begin() + si, st.end()));
-				st.resize(si);
-			}
-			else if (up < me) st.push_back(e);
-			else { /* e is a bridge */ }
+void dfs(int x, int p){
+	dfn[x] = low[x] = ++piv;
+	par[x] = p;
+	for(int i=0; i<graph[x].size(); i++){
+		int w = graph[x][i];
+		if(w == p) continue;
+		if(!dfn[w]){
+			dfs(w, x);
+			low[x] = min(low[x], low[w]);
+		}
+		else{
+			low[x] = min(low[x], dfn[w]);
 		}
 	}
-	return top;
 }
 
-template<class F>
-void bicomps(F f) {
-	num.assign(sz(ed), 0);
-	rep(i,0,sz(ed)) if (!num[i]) dfs(i, -1, f);
+void color(int x, int c){
+	if(c > 0) bcc[x].push_back(c);
+	vis[x] = 1;
+	for(int i=0; i<graph[x].size(); i++){
+		int w = graph[x][i];
+		if(vis[w]) continue;
+		if(dfn[x] <= low[w]){
+			bcc[x].push_back(++cpiv);
+			color(w, cpiv);
+		}
+		else{
+			color(w, c);
+		}
+	}
 }
